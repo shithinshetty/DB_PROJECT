beae4030dac575cbde74e33c742bf21604fe81b8
SHITHIN SHETTY <92690951+shithinshetty@users.noreply.github.com>
2022-01-09 13:39:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="E:\proj\stylesheet\login1.css">
+<title>Super Store Sales Management System</title>
+<style>
+.hero-image {
+  background-image: url("decathlon2.PNG");
+  background-color: ##024b69;
+  height: 600px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: White;
+}
+</style>
+<body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<center>
+
+	<h1><b><u>Super Store Sales Management System</h1>
+</center>	
+<div class="hero-image">
+  <div class="hero-text">
+
+    <h1 style="font-size:70px;color:black"><b>WELCOME</h1>
+    <h2 style="color:white;font-size:40px"><u><b>Login </h2>
+    <a href="admin/dlogin.php"> <button style="font-size:25px ;background-color:#BB8FCE ">Admin Login</button></a>
+    <a href="slogin.php"> <button style="font-size:25px ;background-color:#BB8FCE">Store Login</button></a>
+    <a href="dist/dlogin.php"> <button style="font-size:25px ;background-color:#BB8FCE">Distributor Login</button></a>
+</div>
+</div>
+</body>
+</html>
